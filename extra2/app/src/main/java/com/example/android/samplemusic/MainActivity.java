c293998d470f344e30bc37c56e2a15c55b7491b5
Atif <shafiatifbin@gmail.com>
2020-05-25 23:20:56 -0700

    Uploading a second reference project under 'extra2'

---
+++ extra2/app/src/main/java/com/example/android/samplemusic/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.android.samplemusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Find the View for Now Playing
+        Button now_playing = (Button) findViewById(R.id.play);
+
+        // Set a click listener on that View
+        now_playing.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.color_black);
+
+                // Start the new activity
+                mediaPlayer.start();;
+            }
+        });
+
+
+
+    }
+}
