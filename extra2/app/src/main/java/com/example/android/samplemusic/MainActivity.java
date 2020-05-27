dcd328924251a7a9c920862ef2fb9bb08ee85575
Atif <shafiatifbin@gmail.com>
2020-05-27 00:12:47 -0700

    Added a reference proj: Activity Lifecyle

--- extra2/app/src/main/java/com/example/android/samplemusic/MainActivity.java
+++ extra2/app/src/main/java/com/example/android/samplemusic/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,20 +20,43 @@ public class MainActivity extends AppCompatActivity {
         // Find the View for Now Playing
         Button now_playing = (Button) findViewById(R.id.play);
 
+        // Find the View for Now Playing
+        Button pause = (Button) findViewById(R.id.pause);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.color_black);
+
+
+
         // Set a click listener on that View
         now_playing.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the family category is clicked on.
             @Override
             public void onClick(View view) {
                 // Create a new intent to open the {@link FamilyActivity}
-                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.color_black);
 
                 // Start the new activity
-                mediaPlayer.start();;
+                mediaPlayer.start();
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this, "I'm Done!", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
+        // Set a click listener on that View
+        pause.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
 
+                // Pause the new activity
+                mediaPlayer.pause();
+            }
+        });
 
     }
 }
